3c3653f6d9d3b36d1d895f5f98e9255bb6427239
Jovan Milojevic <109980910+ckobasti77@users.noreply.github.com>
2022-09-01 22:01:26 +0200

    Add files via upload

---
+++ build/precache-manifest.5024874ba1dc260a5466f5f814324a69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea6d442ac4619e9679b5",
+    "url": "/static/css/main.720f502c.chunk.css"
+  },
+  {
+    "revision": "ea6d442ac4619e9679b5",
+    "url": "/static/js/main.ea6d442a.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "877141c665b77609655d",
+    "url": "/static/js/2.877141c6.chunk.js"
+  },
+  {
+    "revision": "c1b6e4f0035755e25821",
+    "url": "/static/js/runtime~main.c1b6e4f0.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "54d533167022d81bea3560ddd33b3d53",
+    "url": "/index.html"
+  }
+];
